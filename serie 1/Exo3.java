1a589272e5f5b798918851f5baaa6f28bbbac145
amir <amir15bfk@gmail.com>
2021-04-24 13:12:55 +0100

    exo 3 Done

---
+++ serie 1/Exo3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+class Exo3{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("give me the first number (A) : ");
+        int A = sc.nextInt();
+        System.out.print("give me the second number (B) : ");
+        int B = sc.nextInt();
+        if (A<=B) {System.out.println("A ("+A+") est le plus petite");}
+        else  {System.out.println("B ("+B+") est le plus petite");}
+        
+    }
+}
